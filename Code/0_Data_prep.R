7df36d57c423626514738c378f4e1736ac0b0cbc
kohrrelation <85494897+kohrrelation@users.noreply.github.com>
2024-07-15 23:29:31 +0200

    Update 0_Data_prep.R

--- Code/0_Data_prep.R
+++ Code/0_Data_prep.R
@@ -1,13 +1,9 @@
 
-## species changes: 
-# count_4, nstop_4, doy_minus_min_4, df_bbs_consider <- cbind(df_routes_year[,1:5])
-
-
 count_k <- paste0('count_', species.no)
 nstop_k <- paste0('nstop_', species.no)
 doy_minus_min_k <- paste0('doy_minus_min_', species.no)
 
-# size of pixel 
+# size of pixel
 size_A <- 20*20
 
 load(paste0("./Data/all-data.RData"))
@@ -28,17 +24,16 @@ n.loc <- nrow(unique(cbind(df_pix_year$x, df_pix_year$y)) )
 
 data_list <- list()
 
-trans_func <- function(x){log((x+365)/(0-x) )}
-trans_func_inv <- function(x){365/-(exp(x)+1)}
+trans_func <- function(x){-log(-(x) / 366)}
+trans_func_inv <- function(x){-366*exp(-x)}
 
-  
 for (indx_year in 1:21){
   data_list$centroidassignment_list[[indx_year]] <- 1:n.loc
   #covariate
   #NAO DATA
   data_list$data[[indx_year]] <- rbind(trans_func(df_pix_year[,doy_minus_min_k][which(df_pix_year$year==2000+indx_year)]),
                                        rep(dat$V3[which(dat$V1==indx_year+2000 & dat$V2== 3)],n.loc) )
-  
+
 }
 
 
@@ -69,7 +64,7 @@ n.sites <- nrow(coords)
 centroids_count_list <- centroids_count_list_2 <- centroids_count_list_bin <- list()
 
 for (indx_year in 1:21){
-  # set the NAs to 0! 
+  # set the NAs to 0!
   # covariate
   centroids_count_list[[indx_year]] <- cbind(coords,df_pix_year$count[which(df_pix_year$year==2000+indx_year)],
                                              rep(indx_year,n.loc ))
@@ -96,13 +91,13 @@ for (indx_year in 1:21){
   durations[indx_year,] <- duration
   #duration[is.na(duration)] <- 0.1
   duration[which(duration==0)] <- NA
-  
+
   n_s[indx_year,] <- df_pix_year$count[which(df_pix_year$year==2000+indx_year)]
   n_s_i[indx_year,] <- df_pix_year[,count_k][which(df_pix_year$year==2000+indx_year)]
-  
+
   centroids_count_list_bin[[indx_year]] <- cbind(coords, df_pix_year[,count_k][which(df_pix_year$year==2000+indx_year)],
                                                  df_pix_year$count[which(df_pix_year$year==2000+indx_year)],
-                                                 duration) 
+                                                 duration)
 }
 
 
@@ -120,8 +115,8 @@ log_like_poissson <- function(y) {
   }
 }
 # Optimization of the loglikelihood
-op_pp <- optimise(log_like_poissson(df_pix_year$count), 
-         interval = c(0, 500), 
+op_pp <- optimise(log_like_poissson(df_pix_year$count),
+         interval = c(0, 500),
          maximum = TRUE)
 
 
@@ -138,27 +133,26 @@ route_length <- apply(df_routes[,-c(1,2)], 2, sum)
 indx.remove <- which(route_length==0)
 
 dat_bbs <- matrix(nrow= n.replicate_gev, ncol=n.route)
-  
+
 for (indx_year in 2001:2021){
   id_lines = which(df_bbs_consider$year == indx_year)
   # merge
   route_id <- data.frame(id_route=(1:n.route))#[-indx.remove] )
-  
+
   df_bbs_consider_2 <- merge(route_id, df_bbs_consider[id_lines,], by=c('id_route'), all.x=TRUE)
-  
+
   # in some years, aggregate multiple routes' counts
   df_bbs_consider_2 <- aggregate(cbind(df_bbs_consider_2$count, df_bbs_consider_2$nstop), by=list(df_bbs_consider_2$id_route), sum)
   colnames(df_bbs_consider_2) <- c('id_route', 'count', 'nstop')
-  
+
   ## turn into 0-1 data
   ind_cnt <- numeric(length=length(df_bbs_consider_2$count))
   ind_cnt[which(df_bbs_consider_2$count>0)] <- 1
-  
+
   dat_bbs[indx_year-2000,] <- df_bbs_consider_2$count/df_bbs_consider_2$nstop
-  
+
   centroids_count_list_2[[indx_year-2000]] <- cbind(df_bbs_consider_2$id_route, df_bbs_consider_2$count, df_bbs_consider_2$nstop)
   #centroids_count_list_2[[indx_year-2000]] <- cbind(df_bbs_consider_2$id_route, ind_cnt, df_bbs_consider_2$nstop)
-  # set the NAs to 0! 
+  # set the NAs to 0!
 }
-
 
